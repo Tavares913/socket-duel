3ab1926394f259dc34fc74cd5e6e14e41ed71161
Tavares913 <jakeknocker74@gmail.com>
2022-05-06 14:54:06 -0400

    changed settings

--- server/settings.js
+++ server/settings.js
@@ -1,13 +1,13 @@
 let settings = {
   playerRadius: 10,
-  playerSpeed: 10,
-  fieldHeight: 200,
-  fieldWidth: 400,
+  playerSpeed: 2,
+  fieldHeight: 500,
+  fieldWidth: 1000,
   pickupRadius: 5,
   orbRadius: 5,
   firstTo: 3,
   maxOrbSpeed: 10,
-  pickupSpawnRate: 2000,
+  pickupSpawnRate: 4000,
   shotCooldown: 2000,
 };
 
