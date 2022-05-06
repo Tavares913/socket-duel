138253fe96e6cdf47d0edcb01dacce4e54439b2f
Tavares913 <jakeknocker74@gmail.com>
2022-05-06 14:45:14 -0400

    fixed react syntax issues

--- client/pages/how-to-play.js
+++ client/pages/how-to-play.js
@@ -19,16 +19,16 @@ const HowToPlay = () => {
           Socket Duel is a 2 player game which involes 2 players battling it out
           in an arena to hit the other first with balls. Every couple of
           seconds, a new ball will spawn, and the first player to pick it up is
-          able to shoot it. If you collide with your own ball, they'll bounce
-          off of you, but if your opponent collides with one of your balls,
-          they'll lose the round. First to win {settings.firstTo} rounds wins
-          the game.
+          able to shoot it. If you collide with your own ball, they&apos;ll
+          bounce off of you, but if your opponent collides with one of your
+          balls, they&apos;ll lose the round. First to win {settings.firstTo}
+          rounds wins the game.
         </p>
         <p>
           To get started, you can either create a room, or join one already
-          created using the room id that that's shown once someone else has
-          created a room. Once both players are in the room, you'll be able to
-          start the round.
+          created using the room id that that&apos;s shown once someone else has
+          created a room. Once both players are in the room, you&apos;ll be able
+          to start the round.
         </p>
         <p>Controls: space=shoot, arrows=move</p>
         <div className={classes["back-home-link"]}>
